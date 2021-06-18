2f3dfb87783aae3f3670937cfc4f2b2bfbcefb4c
Abanoub Shenouda <Abanoub Shenouda>
2021-06-17 20:40:43 -0400

    Add cross site scripting preventing

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,44 +5,16 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// const data = [
-//   {
-//     user: {
-//       name: "Newton",
-//       avatars: "https://i.imgur.com/73hZDYK.png",
-//       handle: "@SirIsaac",
-//     },
-//     content: {
-//       text: "If I have seen further it is by standing on the shoulders of giants",
-//     },
-//     created_at: 1461116232227,
-//   },
-//   {
-//     user: {
-//       name: "Descartes",
-//       avatars: "https://i.imgur.com/nlhLi3I.png",
-//       handle: "@rd",
-//     },
-//     content: {
-//       text: "Je pense , donc je suis",
-//     },
-//     created_at: 1461113959088,
-//   },
-// ];
-
 $(document).ready(function () {
-
   $("#submit-tweet").submit(onSubmit);
   loadTweets();
 });
 
 const loadTweets = () => {
-  $.ajax("/tweets", { method: "GET" })
-    .then(result=>renderTweets(result));
+  $.ajax("/tweets", { method: "GET" }).then((result) => renderTweets(result));
 };
 
 const renderTweets = function (tweets) {
-
   const container = $(".tweet-container");
   container.html("");
 
@@ -62,7 +34,7 @@ const createTweetElement = function (tweet) {
         </div>
         <h5>${tweet.user.handle}</h5>
       </header>
-      <p>${tweet.content.text}</p>
+      <p>${escape(tweet.content.text)}</p>
       <hr>
       <footer>
         <div class="timeago">${timeago.format(tweet.created_at)}</div>
@@ -78,15 +50,23 @@ const createTweetElement = function (tweet) {
 };
 
 const onSubmit = function (event) {
-  //console.log("Event here",event);
   event.preventDefault();
-  const $newtweet = $(this).serialize();
-  //console.log("This is from form", data);
 
-  $.ajax({
-    method: "POST",
-    url: "/tweets",
-    data: $newtweet,
-  })
-    .then(loadTweets());
+  const $newtweet = $(this).serialize();
+  const $newtweetCheck = $newtweet.slice(5);
+
+  
+  if ($newtweet === null || $newtweetCheck === "") {
+    alert("Your tweet can't be empty");
+  } else if ($newtweetCheck.length > 140) {
+    alert("Your tweet can't be more that 140 chars");
+  } else {
+    $.ajax({
+      method: "POST",
+      url: "/tweets",
+      data: $newtweet,
+    })
+      .then(loadTweets())
+      .catch((err) => console.log(err));
+  }
 };
